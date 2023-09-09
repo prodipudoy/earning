a81f2b3c8853b57de137db0f85f0d8177e6e7688
Prodip Udoy <63396894+prodipudoy@users.noreply.github.com>
2023-09-10 00:43:58 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+ <?php
+$url = "https://tokenvolatilebreaker.com/pthrzkya3u?key=0bda29be71b598ad63e0e01dbb6c4e20";
+
+?>
+
+ <!doctype html>
+ <html lang="en">
+
+ <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Natasa Best Videos..</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+     <link rel="icon" href="images/natashavideos.png" sizes="32x32" type="image/png">
+     <style>
+         .main_content a img {
+             padding: 0 20px;
+         }
+
+         .btn_box ul li a {
+             text-decoration: none;
+             color: white;
+             padding: 12px 35px;
+             font-size: 18px;
+         }
+
+         .btn_box ul li a i {
+             padding-right: 5px;
+         }
+
+         .btn_box ul li {
+             margin-top: 15px;
+         }
+     </style>
+ </head>
+
+ <body>
+
+     <div class="container my-5">
+         <div class="row ">
+             <div class="col-12">
+                 <div class="headingBox text-center mb-4">
+                     <h3>Hi I am Natasha, Watch my full video </h3>
+                 </div>
+             </div>
+         </div>
+         <div class="row justify-content-center">
+             <div class="col-lg-5 col-md-6">
+                 <div class="main_content">
+                     <a href="<?php echo $url; ?>">
+                         <img src="images/natasha.jpeg" alt="natasha videos" class="img-fluid">
+                     </a>
+                     <div class="btn_box text-center">
+                         <ul class="list-unstyled">
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-primary"><i
+                                         class="bi bi-play-circle-fill"></i> Play Video</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-secondary"><i
+                                         class="bi bi-cloud-arrow-down-fill"></i> Download Video</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-success"><i
+                                         class="bi bi-whatsapp"></i>Join My Whatsapp</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-primary"><i
+                                         class="bi bi-facebook"></i>Back to Facebook</a> <br>
+                             </li>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-5 col-md-6">
+                 <div class="main_content">
+                     <a href="<?php echo $url; ?>">
+                         <img src="images/natashavideos2.gif" alt="natasha videos" class="img-fluid">
+                     </a>
+                     <div class="btn_box text-center">
+                         <ul class="list-unstyled">
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-primary"><i
+                                         class="bi bi-play-circle-fill"></i> Play Video</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-secondary"><i
+                                         class="bi bi-cloud-arrow-down-fill"></i> Download Video</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-success"><i
+                                         class="bi bi-whatsapp"></i>Join My Whatsapp</a> <br>
+                             </li>
+                             <li>
+                                 <a href="<?php echo $url; ?>" class="btn btn-primary"><i
+                                         class="bi bi-facebook"></i>Back to Facebook</a> <br>
+                             </li>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+         integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+     </script>
+ </body>
+
+ </html>
